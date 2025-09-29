e33742f8ac890af63dd095a8ca6fe385ce61e3f7
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-09-29 19:41:13 +0200

    ajout des request pour valider les donnees

---
+++ app/Http/Requests/Public/CoachRegister.php
@@ -0,0 +1,32 @@
+<?php
+
+namespace App\Http\Requests\Public;
+
+use Illuminate\Foundation\Http\FormRequest;
+
+class CoachRegister extends FormRequest
+{
+    /**
+     * Determine if the user is authorized to make this request.
+     */
+    public function authorize(): bool
+    {
+        return true;
+    }
+
+    /**
+     * Get the validation rules that apply to the request.
+     *
+     * @return array<string, \Illuminate\Contracts\Validation\ValidationRule|array<mixed>|string>
+     */
+    public function rules(): array
+    {
+        return [
+            'email' => 'required|email|unique:coachs,email',
+            'mot_de_passe' => 'required|string|min:8',
+            'nom' => 'required|string',
+            'prenom' => 'required|string',
+            'cip' => 'required|numeric',
+        ];
+    }
+}
