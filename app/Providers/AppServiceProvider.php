4847e2b403f0c95318da9254b150bf309a249299
bouabre225 <158521003+bouabre225@users.noreply.github.com>
2025-09-29 22:06:56 +0200

    ajout de class

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,6 +3,9 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
+use Illuminate\Auth\Events\Verified;
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
+use Illuminate\Auth\Notifications\VerifyEmail;
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -11,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(VerifyEmail::class, SendEmailVerificationNotification::class);
     }
 
     /**
@@ -19,6 +22,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        //
+        $this->app->bind(VerifyEmail::class, SendEmailVerificationNotification::class);
     }
 }
--- bootstrap/providers.php
+++ bootstrap/providers.php
@@ -1,5 +1,7 @@
 <?php
+use Illuminate\Auth\Listeners\SendEmailVerificationNotification;
 
 return [
     App\Providers\AppServiceProvider::class,
+    //SendEmailVerificationNotification::class,
 ];
